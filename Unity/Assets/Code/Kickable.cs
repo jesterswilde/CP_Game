1e09ab02b3827f8a31f309a6625f267f93b03cdf
Corey Wolff <rottenApple@Coreys-MacBook-Air.local>
2017-03-07 09:39:30 +0800

    Kickable working.

--- Unity/Assets/Code/Kickable.cs
+++ Unity/Assets/Code/Kickable.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+	
 public class Kickable : WibblyWobbly, ITargetable {
 
 	[SerializeField]
@@ -13,67 +13,79 @@ public class Kickable : WibblyWobbly, ITargetable {
 	[SerializeField]
 	float _minActivteDistance = 3;
 	[SerializeField]
-	float _collRadius = 1; 
+	float _collRadius = 1f; 
 	Renderer _renderer; 
 	Material _baseMaterial; 
 
 	Vector3 _dir = Vector3.zero;
 	float _speed = 0; 
 	float _minSpeedThreshold = 0.1f; 
-
+	bool _firstPlay = true; 
+	bool _hasCast = false; 
 
 	public override void Play (float _time)
 	{
 		//Debug.Log ("Play"); 
-		if (_speed != 0) {
+		if (_speed != 0 && _firstPlay && !_hasCast) {
+			_firstPlay = false; 
 			Ray _ray = new Ray (transform.position, _dir); 
 			RaycastHit _hit; 
-			float _deltaTime = _time - _prevTime; 
+			float _deltaTime = GameSettings.FixedTimestep;
 			float _travelDist = _speed * _deltaTime; 
 			if (Physics.Raycast (_ray, out _hit, _collRadius + _travelDist, _collMask)) {
+				_hasCast = true; 
 				Vector3 _travelVec = _dir * _speed * _deltaTime;
 				float _toCollision = _hit.distance - _collRadius;
+				Debug.Log ("Dist " + _toCollision / _travelDist * _deltaTime + " | " + _deltaTime + " | " + _time ); 
 				float _collTime = (_toCollision / _travelDist) * _deltaTime + _time;
-				Vector3 _newDir = Vector3.Reflect (_travelVec, _hit.normal).normalized; 
-				float _newSpeed = _speed - ModSpeed (_collTime - _time); 
-				SetExternalAction (new VectorAction (ActionType.Activate, _newSpeed * _newDir, _collTime, true)); 
+				Vector3 _newDir = Vector3.Reflect (_travelVec, _hit.normal).normalized;
+				Debug.Log (GameManager.FixedGameTime + " | " + _collTime); 
+				SetExternalAction(new VectorAction(ActionType.Deactivate, _dir * _speed, true));
+				SetExternalAction(new DirTargetAction (ActionType.Activate, _speed * _newDir, transform.position , true)); 
 			}
 		}
 		base.Play (_time);
+		_firstPlay = true; 
 	}
-	float ModSpeed(float _deltaTime){
-		if (_speed < _minSpeedThreshold) {
+	int ModSpeed(float _deltaTime){
+		if (_speed < _minSpeedThreshold && _speed != 0) {
 			SetExternalAction (new ValueAction (ActionType.ThresholdReached, _speed)); 
-			return _speed;  
+			return (int)_speed;  
 		} else {
-			return _speed * _drag * _deltaTime; 
+			int _intSpeed = (int)(_speed * 10000); 
+			return Mathf.FloorToInt(_intSpeed * _drag * _deltaTime); 
 		}
 	}
 
 	protected override void Act (float _deltaTime)
 	{
-		transform.position += _speed * _dir * _deltaTime;
-		_speed -= ModSpeed (_deltaTime); 
+		if (_speed != 0) {
+			Debug.Log (_deltaTime + " | " + _speed); 
+			int _intSpeed = Mathf.FloorToInt(_speed * 10000); 
+			_intSpeed -= ModSpeed (_deltaTime);	
+			_speed = _intSpeed / 10000f; 
+			transform.position += _speed * _dir * _deltaTime;
+		}
 	}
 	
-	protected override void ActReverse (float _deltaTIme)
+	protected override void ActReverse (float _deltaTime)
 	{
-		transform.position -= _speed * _dir * _deltaTIme; 
-		_speed += ModSpeed (_deltaTIme); 
+		if(_speed != 0){
+			int _intSpeed = Mathf.FloorToInt(_speed * 10000); 
+			_intSpeed += ModSpeed (_deltaTime);
+			_speed = _intSpeed / 10000f; 
+			transform.position -= _speed * _dir * _deltaTime; 
+		}
 	}
 
 	protected override void UseAction (Action _action, float _time)
 	{
-		//Debug.Log (_action.Type); 
+		Debug.Log (_action.Type); 
 		switch (_action.Type) {
 		case(ActionType.Activate):
-			SetExternalAction(new VectorAction(ActionType.Deactivate, _dir * _speed, _time, true); 
 			_speed = _action.Vector.magnitude;
 			_dir = _action.Vector.normalized;
-			Debug.Log (_speed + " | " + _dir); 
-			break;
-		case(ActionType.ThresholdReached):
-			_speed = 0; 
+			_hasCast = false;
 			break;
 		}
 	}
@@ -81,8 +93,9 @@ public class Kickable : WibblyWobbly, ITargetable {
 	{
 		switch (_action.Type) {
 		case(ActionType.Deactivate):
+			transform.position = _action.OriginalVec; 
 			_speed = _action.Vector.magnitude; 
-			_dir = _action.Vector.normalized; 
+			_dir = _action.Vector.normalized;
 			break; 
 		case(ActionType.ThresholdReached):
 			_speed = _action.Value;
@@ -106,10 +119,11 @@ public class Kickable : WibblyWobbly, ITargetable {
 	}
 	public List<Action> Activate (Character _character)
 	{
-		Debug.Log ("kicking"); 
+		Debug.Log (_character.transform.position + " | " + transform.position); 
 		Vector3 _kick = transform.position - _character.transform.position; 
-		_kick = new Vector3 (_dir.x, 0, _dir.y).normalized * _force; 
+		_kick = new Vector3 (_kick.x, 0, _kick.z).normalized * _force; 
 		SetAction(new VectorAction (ActionType.Activate, _kick, true));  
+		SetAction(new DirTargetAction(ActionType.Deactivate, _dir * _speed, transform.position, true)); 
 		return null; 
 	}
 	public void RewindActivation (Action _action)
@@ -128,7 +142,7 @@ public class Kickable : WibblyWobbly, ITargetable {
 		_baseMaterial = _renderer.material; 
 	}
 	void Start(){
-		_history.AddToHead(new BasicAction(ActionType.Null));
-		//RegisterWibblyWobbly();
+		SetAction(new BasicAction(ActionType.Null));
+		RegisterWibblyWobbly();
 	}
 }
